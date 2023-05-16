cc6d6463b11fbd634218232d54d654500da6eb9c
ChatBypass <113918659+ChatBypass@users.noreply.github.com>
2023-05-16 13:19:17 +0200

    Official release

---
+++ RIG.bat
@@ -0,0 +1,10 @@
+echo off
+cls
+color 0a
+cls
+title RIG (Roblox Item Granter) [NEW]
+echo Roblox Item Granter
+echo Made by Tritemis
+set /p assetid=Insert Asset ID: 
+start www.roblox.com/catalog/%assetid%/🆘💥🕳/?AssetGranted=true
+start RIG.bat
